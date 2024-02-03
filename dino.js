4b133cead3068352afe7ff6a4145920f94d8457d
Michael Sam <sam.michael.h@gmail.com>
2024-02-03 18:07:10 +0900

    Add losing function and restart game function in js

--- dino.js
+++ dino.js
@@ -26,6 +26,14 @@ export function updateDino(delta, speedScale) {
   handleJump(delta)
 }
 
+export function getDinoRect() {
+  return dinoElem.getBoundingClientRect()
+}
+
+export function setDinoLose() {
+  dinoElem.src = "images/dino-lose.png"
+}
+
 function handleRun(delta, speedScale) {
   if (isJumping) {
     dinoElem.src = `images/dino-stationary.png`
